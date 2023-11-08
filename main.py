dee52bfa1819a6a4c79145de3aa3c824502222b9
marceloaviana <96154514+marceloaviana@users.noreply.github.com>
2023-11-08 18:26:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Exercicio 23
+
+class Aluno:
+  def __init__(self, nome, matricula, curso):
+    self.nome = nome
+    self.matricula = matricula
+    self.curso = curso
+
+#dados_aluno = Aluno(input("Digite o nome do aluno: "), input("Digite a matricula do aluno: "), input("Digite o curso do aluno: "))
+
+dados_aluno = Aluno("Marcelo Viana", "123456", "Engenharia Elética")
+
+print("nome: ", dados_aluno.nome)
+print("matricula: ", dados_aluno.matricula)
+print("curso: ", dados_aluno.curso)
